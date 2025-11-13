49c7b7b76db038a7b8e2172157778ddc7533d4a7
NataliaKosh <nataliakosh17@gmail.com>
2025-11-14 01:11:23 +0300

    correction

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -186,7 +186,7 @@ class RecipeAdmin(admin.ModelAdmin):
 
     @admin.display(description="В избранном")
     def favorites_count_display(self, recipe):
-        return recipe.favorite__user.count()
+        return recipe.favorite_set.count()
 
     @admin.display(description="В корзинах")
     def in_shopping_carts_count_display(self, recipe):
