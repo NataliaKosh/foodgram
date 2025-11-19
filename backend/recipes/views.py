51538a278e8f3b42344e3c6a89d14d81597dce03
NataliaKosh <nataliakosh17@gmail.com>
2025-11-19 21:21:21 +0300

    corrections

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -1,10 +1,18 @@
-from django.http import Http404
+import short_url
+
 from django.shortcuts import redirect
 
 from .models import Recipe
 
 
-def short_link_redirect(request, pk):
-    if not Recipe.objects.filter(pk=pk).exists():
-        raise Http404(f"Рецепт с id {pk} не найден")
-    return redirect(f'/recipes/{pk}/')
+def short_link_redirect(request, short_code: str):
+    try:
+        recipe_id = short_url.decode_url(short_code)
+
+        if not Recipe.objects.filter(pk=recipe_id).exists():
+            return redirect('/not_found/')
+
+        return redirect(f'/recipes/{recipe_id}/')
+
+    except (ValueError, KeyError):
+        return redirect('/not_found/')
