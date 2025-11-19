6153a059750199d7e5b755c9205b57ddeef77c93
NataliaKosh <nataliakosh17@gmail.com>
2025-11-19 21:05:12 +0300

    corrections

--- backend/recipes/urls.py
+++ backend/recipes/urls.py
@@ -4,6 +4,7 @@ from .views import short_link_redirect
 
 app_name = 'recipes'
 
+
 urlpatterns = [
-    path('s/<str:short_code>/', short_link_redirect, name='recipe-short-link'),
+    path('s/<int:pk>/', short_link_redirect, name='short-link'),
 ]
